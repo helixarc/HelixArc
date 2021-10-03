cf13e1ae6ab4fefc6b7fedf338f3233aa387f910
Ethan Cook <cook.ethan.j@gmail.com>
2021-10-03 19:14:49 -0400

    chore(init): Initial Project Commit

---
+++ Source/HelixArc/HelixArc.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HelixArc.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HelixArc, "HelixArc" );

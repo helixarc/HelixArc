01ee7135c472420e3e20da3df6c06fb228565252
Ethan Cook <cook.ethan.j@gmail.com>
2021-10-09 16:22:08 -0400

    feat(player): Initial Player Class
    - Created base player character and controller classes from ALS.
    - Created initial player HUD UI

---
+++ Source/HelixArc/Player/HelixArcCameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ALSPlayerCameraManager.h"
+#include "HelixArcCameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELIXARC_API AHelixArcCameraManager : public AALSPlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};
